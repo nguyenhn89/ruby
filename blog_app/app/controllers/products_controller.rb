7ce38ce037626e2523a459529ad1c232f5f24b15
nguyenhn.nta <nguyenhn@mail.nitrotech.asia>
2026-04-24 09:01:42 +0700

    add blog_app

---
+++ blog_app/app/controllers/products_controller.rb
@@ -0,0 +1,49 @@
+class ProductsController < ApplicationController
+  before_action :set_product, only: %i[show edit update destroy]
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to @product, notice: "Tạo sản phẩm thành công."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to @product, notice: "Cập nhật sản phẩm thành công."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path, notice: "Xóa sản phẩm thành công."
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price, :stock)
+  end
+end
---
+++ blog_app/config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  resources :products
+
+  # get    "/products",          to: "products#index",   as: :products        # danh sách
+  # get    "/products/new",      to: "products#new",     as: :new_product     # form tạo mới
+  # post   "/products",          to: "products#create"                        # lưu mới
+  # get    "/products/:id",      to: "products#show",    as: :product         # xem chi tiết
+  # get    "/products/:id/edit", to: "products#edit",    as: :edit_product    # form sửa
+  # patch  "/products/:id",      to: "products#update"                        # lưu sửa
+  # delete "/products/:id",      to: "products#destroy"                       # xóa
+end
